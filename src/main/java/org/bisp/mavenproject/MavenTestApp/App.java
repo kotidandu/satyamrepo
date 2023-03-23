26cc7c31a40eb2e1b4b7a9ab4c18100bdc7ff053
dev1 <dev1@gmail.com>
2023-03-23 10:24:37 +0530

    first commit

---
+++ src/main/java/org/bisp/mavenproject/MavenTestApp/App.java
@@ -0,0 +1,13 @@
+package org.bisp.mavenproject.MavenTestApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "First maven project" );
+    }
+}
---
+++ src/main/java/org/bisp/mavenproject/MavenTestApp/TestClass.java
@@ -0,0 +1,11 @@
+package org.bisp.mavenproject.MavenTestApp;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		 System.out.println("welcome to maven project");
+		// TODO Auto-generated method stub
+
+	}
+
+}
