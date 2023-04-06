f02cd2c33d4d0e21794a4115bac6c36694e80ded
dev1 <dev1@gmail.com>
2023-04-06 13:01:20 +0530

    commited

--- src/main/java/org/bisp/mavenproject/MavenTestApp/TestClass.java
+++ src/main/java/org/bisp/mavenproject/MavenTestApp/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		 System.out.println("welcome to maven project");
+		 System.out.println ("welcome to the jenkinsfile");
 		// TODO Auto-generated method stub
 
 	}
