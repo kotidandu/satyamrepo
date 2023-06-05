a7d24fec57043830817ee87481a649f2d7f58583
dev1 <dev1@gmail.com>
2023-06-05 16:46:17 +0530

    committed

--- src/main/java/org/bisp/mavenproject/MavenTestApp/App.java
+++ src/main/java/org/bisp/mavenproject/MavenTestApp/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "First maven project" );
+        System.out.println ("moved to github account");
     }
 }
